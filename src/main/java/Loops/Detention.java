b11d4a65f25e68039e098f60374797d04e5b481a
Nazarii Soviak <natesoviak@gmail.com>
2022-09-29 21:36:23 +0300

    Initial commit

---
+++ src/main/java/Loops/Detention.java
@@ -0,0 +1,17 @@
+package Loops.workbooks;
+
+import java.util.Scanner;
+
+public class Detention {
+    //Task 1 – Scan for the nextLine(), and print it 99 times.
+
+    public static void main(String[] args) {
+        System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        for (int i = 0; i <= 99; i++) {
+            System.out.println(i + ". " + input);
+        }
+    }
+}
